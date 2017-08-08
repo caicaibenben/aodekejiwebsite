3e0ea76f856ae344a42f2782d319074692d576f5
caicaibenben <179040604@qq.com>
2017-08-08 22:57:17 +0800

    test

---
+++ 12.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <!-- 新 Bootstrap 核心 CSS 文件 -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
+    <script src="jquery/jquery-3.2.1.min.js"></script>
+    <!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+    <script src="bootstrap/css/bootstrap.min.js"></script>
+    <script src="Test.js"></script>
+    <link rel="stylesheet" href="Test.css" type="text/css">
+    <link rel="stylesheet" href="base.css" type="text/css">
+</head>
+<body>
+<div class="intro">
+    <div class="">
+        <div class="">
+            <div class="phone_show">
+              <div class="">
+                <div class="right">
+                    <!-- 左侧按钮-->
+                    <div class="left">
+                        <button type="button" class="btn btn-default btn-lg btn_left">
+                            <span class="glyphicon glyphicon-chevron-left"></span>
+                        </button>
+                    </div>
+                    <!--手机屏幕-->
+                    <div class="mid_check left">
+                        <img class="" src="image/phone_border.jpg" width="273" height="528">
+                        <div class="img_out_box left">
+                            <div class="img_out left" id="img_out">
+                                <img src="image/pic.png" width="237">
+                                <img src="image/pic.png" width="237">
+                                <img src="image/pic.png" width="237">
+                                <img src="image/pic.png" width="237">
+                            </div>
+                        </div>
+                    </div>
+                    <!--右侧按钮-->
+                    <div class="left">
+                        <button type="button" class="btn btn-default btn-lg btn_right" style="margin-right: 80px">
+                            <span class="glyphicon glyphicon-chevron-right"></span>
+                        </button>
+                    </div>
+                </div>
+               </div>
+            </div>
+            <div class="text_out_sider left">
+                <!--内容介绍-->
+                <div class="text_out_box">
+                    <div class="text_img_box">
+                        <h4><img src="image/word-1.png" class="img-responsive text_img" ></h4>
+                    </div>
+                    <div class="text_img_box none">
+                        <h4><img src="image/word-2.png" class="img-responsive text_img" ></h4>
+                    </div>
+                    <div class="text_img_box none">
+                        <h4><img src="image/word-3.png" class="img-responsive text_img" ></h4>
+                    </div>
+                    <div class="text_img_box none">
+                        <h4><img src="image/word-4.png" class="img-responsive text_img" ></h4>
+                    </div>
+                </div>
+                <!--图标按钮-->
+                <p class="icon-out"  id="icon-out" style="margin-top:50px;">
+                    <a class="icon"><img src="image/book_active.png" class="img-responsive" alt="0"></a>
+                    <a class="icon"><img src="image/book.png" class="img-responsive" alt="1"></a>
+                    <a class="icon"><img src="image/book.png" class="img-responsive" alt="2"></a>
+                    <a class="icon"><img src="image/book.png" class="img-responsive" alt="3"></a>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
