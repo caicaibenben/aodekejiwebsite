f6f8f2e6afb9e06331706058059c318484814554
caicaibenben <179040604@qq.com>
2017-08-15 06:56:40 +0800

    Modify

--- Test.php
+++ Test.php
@@ -23,7 +23,7 @@
         <div class="col-md-5 col-md-offset-1" style="background-color: #e6e6e6;">
             <!--地图显示-->
             <div id="siteMap">
-            <!--显示百度地图-->
+                <!--显示百度地图-->
             </div>
         </div>
         <div class="col-md-3 col-md-offset-1" style="background-color: #e6e6e6" >
@@ -32,7 +32,7 @@
                 <form class="bs-example bs-example-form" role="form">
                     <div class="col-lg-12">
                         <div class="input-group">
-                            <div class="input-group-btn">
+                            <div class="input-group-btn" id="dropdownmutilmenu">
                                 <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
                                     全部
                                     <span class="caret"></span>
@@ -64,7 +64,6 @@
                                                 } else {
                                                     echo '<li><a  href="javascript:;">'.$shi.'</a></li>';
                                                 }
-
                                             }
                                             echo '</ul></li>';
                                         } else {
@@ -73,7 +72,7 @@
                                     }
                                     ?>
                             </div><!-- /btn-group -->
-                            <input type="text" class="form-control" placeholder="请输入你要查询的站点">
+                            <input id="SearchText" type="text" class="form-control" placeholder="请输入你要查询的站点">
                             <span class="input-group-btn" id="searchbtn">
                                     <button id="searchbtn" class="btn btn-default" type="button">
                                         <span class="glyphicon glyphicon-search"> </span>
@@ -85,158 +84,23 @@
             </div>
             <!--站点列表-->
             <div style="margin-top: 100px">
-                <div class="list-group">
-                    <a href="javascript:;" class="list-group-item active " data-toggle="collapse"
-                       data-target="#demo0">
-                        <h4 class="list-group-item-heading">
-                            紫禁城小区
-                        </h4>
-                    </a>
-                    <div id="demo0" class="collapse">
-                        详细地址：1111111111111111111111111111111111111
-                        11111111111111111111111111111111111111111111111
-                    </div>
-                    <a href="javascript:;" class="list-group-item active " data-toggle="collapse"
-                       data-target="#demo1">
-                        <h4 class="list-group-item-heading">
-                            紫禁城小区
-                        </h4>
-                    </a>
-                    <div id="demo1" class="collapse">
-                        详细地址：1111111111111111111111111111111111111
-                        11111111111111111111111111111111111111111111111
-                    </div>
-                    <a href="javascript:;" class="list-group-item " data-toggle="collapse"
-                       data-target="#demo2">
-                        <h4 class="list-group-item-heading">
-                            紫禁城小区
-                        </h4>
-                    </a>
-                    <div id="demo2" class="collapse">
-                        详细地址：1111111111111111111111111111111111111
-                        11111111111111111111111111111111111111111111111
-                    </div>
-                    <a href="javascript:;" class="list-group-item" data-toggle="collapse"
-                       data-target="#demo3" >
-                        <h4 class="list-group-item-heading">
-                            紫禁城小区
-                        </h4>
-                        <div id="demo3" class="collapse">
-                            详细地址：1111111111111111111111111111111111111
-                            11111111111111111111111111111111111111111111111
-                        </div>
-                    </a>
-
+                <div id="list-group" class="list-group pre-scrollable">
+                    <!--                    <a href="javascript:;" class="list-group-item active " data-toggle="collapse"-->
+                    <!--                       data-target="#demo0">-->
+                    <!--                        <h4 class="list-group-item-heading">-->
+                    <!--                            紫禁城小区-->
+                    <!--                        </h4>-->
+                    <!--                    </a>-->
+                    <!--                    <div id="demo0" class="collapse">-->
+                    <!--                        详细地址：1111111111111111111111111111111111111-->
+                    <!--                        11111111111111111111111111111111111111111111111-->
+                    <!--                    </div>-->
+                    <!--                    <a id="forSiteList"></a>-->
                 </div>
 
             </div>
         </div>
     </div>
-
 </section>
-<script type="text/javascript">
-    var points= new Array();
-    points[0]=new BMap.Point(118.383737 ,35.130189 );
-    points[1]=new BMap.Point(118.4102 ,35.278136 );
-    points[2]=new BMap.Point(117.966885 ,35.278489 );
-    points[3]=new BMap.Point(119.147483 ,35.131858 );
-    points[4]=new BMap.Point(116.404, 39.915);
-    function initMap() {
-        // 百度地图初始化
-        var map = new BMap.Map("siteMap");    // 创建Map实例
-        map.centerAndZoom(new BMap.Point(116.404, 39.915), 12);  // 初始化地图,设置中心点坐标和地图级别
-        map.addControl(new BMap.MapTypeControl());   //添加地图类型控件
-        //map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
-        function myFun(result){
-            var cityName = result.name;
-            map.setCenter(cityName);
-        }
-        var myCity = new BMap.LocalCity();
-        myCity.get(myFun);//获取定位地址之后必须通过get来回掉函数
-        map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
-    }
-
-    //百度地图显示更新 points并加注标签
-    function refreshMap(points) {
-        console.log("refresh");
-        var map = new BMap.Map("siteMap");    // 创建Map实例
-        var view = map.getViewport(eval(points));
-        var mapZoom = view.zoom;
-        console.log("mapZoom"+mapZoom);
-        var centerPoint = view.center;
-        console.log("centerPoint"+centerPoint.lng+" "+centerPoint.lat);
-        map.centerAndZoom(centerPoint,mapZoom);
-        map.enableScrollWheelZoom(true);
-        for(var i=0;i<points.length;i++){
-            var redIcon = new BMap.Icon("image/mapbiaozhu.png", new BMap.Size(21, 33), {
-                // 指定定位位置。
-                // 当标注显示在地图上时，其所指向的地理位置距离图标左上
-                // 角各偏移10像素和25像素。您可以看到在本例中该位置即是
-                // 图标中央下端的尖角位置。
-                anchor: new BMap.Size(10, 16),
-                // 设置图片偏移。
-                // 当您需要从一幅较大的图片中截取某部分作为标注图标时，您
-                // 需要指定大图的偏移位置，此做法与css sprites技术类似。
-                imageOffset:new BMap.Size(i*(-21),0)   // 设置图片偏移
-            });
-            var marker = new BMap.Marker(points[i],{icon:redIcon});
-            map.addOverlay(marker);
-        }
-    }
-
-    refreshMap(points);
-    //特定点的地图显示
-    function refreshMapPoint(points,point) {
-        console.log("refresh");
-        var map = new BMap.Map("siteMap");    // 创建Map实例
-        var view = map.getViewport(eval(points));
-        var mapZoom = view.zoom;
-        console.log("mapZoom"+mapZoom);
-        var centerPoint = view.center;
-        console.log("centerPoint"+centerPoint.lng+" "+centerPoint.lat);
-        map.centerAndZoom(centerPoint,mapZoom);
-        map.enableScrollWheelZoom(true);
-        for(var i=0;i<points.length;i++){
-            if(points[i]==point){
-                var myIcon = new BMap.Icon("image/mapblue.png", new BMap.Size(21, 33), {
-                    // 指定定位位置。
-                    // 当标注显示在地图上时，其所指向的地理位置距离图标左上
-                    // 角各偏移10像素和25像素。您可以看到在本例中该位置即是
-                    // 图标中央下端的尖角位置。
-                    anchor: new BMap.Size(10, 16),
-                });
-
-                var marker_point = new BMap.Marker(point,{icon:myIcon});
-                map.addOverlay(marker_point);
-                continue;
-            }
-            var redIcon = new BMap.Icon("image/mapbiaozhu.png", new BMap.Size(21, 33), {
-                // 指定定位位置。
-                // 当标注显示在地图上时，其所指向的地理位置距离图标左上
-                // 角各偏移10像素和25像素。您可以看到在本例中该位置即是
-                // 图标中央下端的尖角位置。
-                anchor: new BMap.Size(10, 16),
-                // 设置图片偏移。
-                // 当您需要从一幅较大的图片中截取某部分作为标注图标时，您
-                // 需要指定大图的偏移位置，此做法与css sprites技术类似。
-                imageOffset:new BMap.Size(i*(-21),0)   // 设置图片偏移
-            });
-            var marker = new BMap.Marker(points[i],{icon:redIcon});
-            map.addOverlay(marker);
-        }
-    }
-    $("#searchbtn").click(function(){
-//        var point=points[0];
-//        refreshMapPoint(points,point);
-        $.post("RawTest.php",
-           {},
-            function(data,status){
-                var obj = JSON.parse(data);
-                //var obj = eval(data);
-                console.log(obj);
-                alert(status);
-            });
-    });
-</script>
 </body>
 </html>
